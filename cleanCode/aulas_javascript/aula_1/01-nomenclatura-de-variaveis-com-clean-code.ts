c5578063ca20fd538c950b03ccfbaaea17acd1fc
Merces-Dev <giovanitjs013@gmail.com>
2022-12-01 11:26:23 -0300

    Primeira aula: Nomenclatura de variaveis e clean code

---
+++ cleanCode/aulas_javascript/aula_1/01-nomenclatura-de-variaveis-com-clean-code.ts
@@ -0,0 +1,55 @@
+// Nomenclatura de variáveis
+
+const userCategoriesRank = [
+    {
+      title: 'User',
+      followers: 5
+    },
+    {
+      title: 'Friendly',
+      followers: 50,
+    },
+    {
+      title: 'Famous',
+      followers: 500,
+    },
+    {
+      title: 'Super Star',
+      followers: 1000,
+    },
+  ]
+  
+  export default async function getUserData(req, res) {
+    const username = String(req.query.username)
+  
+    if (!username) {
+      return res.status(400).json({
+        message: `Please provide an username to search on the github API`
+      })
+    }
+  
+    const userDataResponse = await fetch(`https://api.github.com/users/${username}`);
+  
+    if (userDataResponse.status === 404) {
+      return res.status(400).json({
+        message: `User with username "${username}" not found`
+      })
+    }
+  
+    const userData = await userDataResponse.json()
+  
+    const orderedUserCategoriesRank = userCategoriesRank.sort((a, b) =>  b.followers - a.followers); 
+  
+    const userCategory = orderedUserCategoriesRank.find(userCategoryItem => userData.followers > userCategoryItem.followers)
+  
+    const result = {
+      username,
+      category: userCategory?.title
+    }
+  
+    return result
+  }
+  
+  getUserData({ query: {
+    username: 'josepholiveira'
+  }}, {})
