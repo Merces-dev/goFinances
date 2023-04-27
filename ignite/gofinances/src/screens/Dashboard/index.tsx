ee9af71ace9536ed8820ea4f624bbfa9b3470ad5
Merces-Dev <giovanitjs013@gmail.com>
2023-04-26 23:06:36 -0300

    Gofinances-01: Initiates use of Styled-components

---
+++ ignite/gofinances/src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+import * as St from "./styles";
+export function Dashboard() {
+  return (
+    <St.Container>
+      <St.Title>Dashboard</St.Title>
+    </St.Container>
+  );
+}
