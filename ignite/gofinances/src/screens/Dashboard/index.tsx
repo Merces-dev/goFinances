08639c5eabdcfb9cef87cee6b2f61b0458e06262
Merces-Dev <giovanitjs013@gmail.com>
2023-04-28 00:16:02 -0300

    Gofinances-03: Initiates use of proportional measures

--- ignite/gofinances/src/screens/Dashboard/index.tsx
+++ ignite/gofinances/src/screens/Dashboard/index.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { Text } from "react-native";
 import * as St from "./styles";
+
 export function Dashboard() {
   return (
     <St.Container>
-      <St.Title>Dashboard</St.Title>
+      <St.Header>
+        <St.HeaderWrapper>
+          <St.UserInfo>
+            <St.Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/61596432?v=4",
+              }}
+            />
+            <St.User>
+              <St.UserGreeting>Olá,</St.UserGreeting>
+              <St.UserName>Giovani</St.UserName>
+            </St.User>
+          </St.UserInfo>
+          <St.Icon name="power" />
+        </St.HeaderWrapper>
+      </St.Header>
     </St.Container>
   );
 }
